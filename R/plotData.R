57e0e02e8dac27f0b2c31e2728019532d2b0066c
Sebastian <szugat@statistik.tu-dortmund.de>
2015-04-28 11:59:53 +0200

    Added plotData to exported functions

--- R/plotData.R
+++ R/plotData.R
@@ -4,6 +4,7 @@
 #' @param t values for y-axis
 #' @param ... further arguments passed to plot()
 #' @return plot of x and log10(t)
+#' @export
 plotData <- function(x, t, ...){
   plot(x, log10(t), ylim = c(0, 10), pch = 19, 
        ylab = expression(log[10](t)), xlab = expression(s %.% L[max]/(L[max]-j)), ...)
