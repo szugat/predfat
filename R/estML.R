f43b691e7c767cd152fd1de18d85f56bee68d0b8
Sebastian <szugat@statistik.tu-dortmund.de>
2015-07-23 15:57:23 +0200

    - maximum likelihood estimation for censored data

--- R/estML.R
+++ R/estML.R
@@ -1,22 +1,27 @@
 #' Maximum likelihood estimation of parameters in link function for the exponential distribution
 #' 
-#' @param link a function(theta, log) used in the likelihood which is \eqn{L(\theta) = \sum t * \lambda(x) - log(\lambda(x))}, where log = TRUE refers to term at the end
 #' @param x values of influental variable for the link function
 #' @param t values of dependent variable
+#' @param delta vector indicating of length length(t) indicating if the respective observation in t is completely observed (1) or censored (0)
+#' @param link a function(theta, log) used in the likelihood which is \eqn{L(\theta) = \sum t * \lambda(x) - log(\lambda(x))}
 #' @param start starting solution for optim()
 #' @param ... further paraments passed to optim()
 #' @return optimal solution for the parameter in the link function in optimum and estimated 
 #'         information matrix in I
 #' @export
-estML <- function(link, type = 1, x, t, start, ...){
+estML <- function(x, t, delta, link, type = 1, start, ...){
   ## link function
   if (missing(link)) {
     link <- linkfun(type)
   }
-  
+  ## if delta is not given assume that no observations are censored:
+  if (missing(delta)) {
+    delta <- rep(1, length(t))
+  }
   ## objective function
   loglike <- function(theta){
-    sum(t * exp(link(theta = theta, x = x)) - link(theta = theta, x = x))
+   sum(delta * (t * exp(link(theta = theta, x = x)) - link(theta = theta, x = x))) + # density function
+      sum((1 - delta) * exp(-link(theta = theta, x = x)) * t) # survival function
   }
   thetahat <- optim(par = start, fn = loglike, ...)
   information <- predfat:::estI(x, theta = thetahat$par)
