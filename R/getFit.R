6e2b8210f1e0dc6752b1ed9131baac5da4f384cd
Sebastian <szugat@statistik.tu-dortmund.de>
2015-08-18 19:05:04 +0200

    Added the possibility of arbitrary link functions

--- R/getFit.R
+++ R/getFit.R
@@ -1,15 +1,23 @@
 #' Computation of fitted values for new data
 #' @param newdata values of influental variable
 #' @param theta parameter vector for link function
+#' @param lambda [\code{function(theta, x)}]\cr
+#'        link function for exponential distribution
+#' @param type [\code{integer}]\cr
+#'        if link function is not given a collection of given link function is available, see \code{\link{linkfun}}
 #' @param plot logical value indicating whether the fitted values should be added to an existing plot
 #' @param median logical value indicating whether the median (TRUE) oder the expectation should be fitted (FALSE, default)
 #' @return vector of fitted values
-getFit <- function(newdata, theta, plot = FALSE, median = FALSE){
+getFit <- function(newdata, theta, lambda, type, plot = FALSE, median = FALSE){
+  if (missing(lambda)) {
+    lambda <- linkfun(type)
+  }
   if (length(theta) == 2) {
     theta <- c(theta, 0, 1)
   }
   
-  fitted <- (theta[1] - theta[2]*(newdata) + theta[3] * (newdata)^(-theta[4]))/log(10)
+  fitted <- lambda(x = newdata, theta = theta)/log(10)
+  
   if (median) {
     fitted <- log10(log(2)) + fitted
   }
