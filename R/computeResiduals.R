4018d4c56e2cfdb0804cb9fdac782149eec80a07
Sebastian <szugat@statistik.tu-dortmund.de>
2015-08-18 17:35:23 +0200

    Changes for new structure of the link functions

--- R/computeResiduals.R
+++ R/computeResiduals.R
@@ -3,7 +3,16 @@
 #' @param theta numeric vector of length two or four with link function's parameters
 #' @param x values of influental variable for the link function
 #' @param y values of dependent variable
+#' @param lambda [\code{function(theta, x)}]\cr
+#'        link function for exponential distribution
+#' @param gradient [\code{function(x, theta, ...)}]\cr
+#'        gradient of link function
+#' @param type [\code{integer}]\cr
+#'        if link function is not given a collection of given link function is available, see \code{\link{linkfun}}
 #' @return vector of residuals
-computeResiduals <- function(theta, x, y) {
-  y - log(2) * 1 / lambda(x = x, theta = theta)
+computeResiduals <- function(theta, x, y, lambda, type) {
+  if (missing(lambda)) {
+    lambda <- linkfun(type)
+  }
+  y - log(2) * 1 / exp(lambda(x = x, theta = theta))
 }
