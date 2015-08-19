132d8f0094edcc4f6a5652fb44defb09af5d9943
Sebastian <szugat@statistik.tu-dortmund.de>
2015-08-19 12:38:39 +0200

    Adjusted to linkfun.R

--- R/gradLambda.R
+++ R/gradLambda.R
@@ -1,24 +1,29 @@
-#' Gradient of link function with respect to parameter theta
-#' @param x value of influental variable for the link function
-#' @param theta numeric vector of length four with link function's parameters
+#' Collection of gradient functions of link function for exponential distribution
+#' @param type integer (1-6) indicating the desired link function, see \code{\link{linkfun}}
 #' @return gradient of link function at x with respect to theta 
 gradLambda <- function(type = 4) {
-  lambda <- linkfun(type)
-  lambdaValue <- exp(lambda(x, theta))
   if (type == 1 || type == 2) {
-    lambdaDot <- cbind(-lambdaValue,
-                       x * lambdaValue,
-                       -x^(-theta[4]) * lambdaValue,
-                       theta[3]*x^(-theta[4]) * lambdaValue * log(x))
-    if ((length(theta) == 2) || theta[3] == 0) {
-      lambdaDot <- lambdaDot[,1:2]
+    res <- function(x, theta) {
+      lambda <- linkfun(type)
+      lambdaValue <- exp(lambda(x, theta))
+      lambdaDot <- cbind(-lambdaValue,
+                         x * lambdaValue,
+                         -x^(-theta[4]) * lambdaValue,
+                         theta[3]*x^(-theta[4]) * lambdaValue * log(x))
+      if ((length(theta) == 2) || theta[3] == 0) {
+        lambdaDot <- lambdaDot[,1:2]
+      }
     }
   }
   
   if (type == 4) {
-    lambdaDot <- cbind(-lambdaValue,
-                       lambdaValue * x,
-                       lambdavalue * log(x))
+    res <- function(x, theta) {
+      lambda <- linkfun(type)
+      lambdaValue <- exp(lambda(x, theta))
+      lambdaDot <- cbind(-lambdaValue,
+                         lambdaValue * x,
+                         lambdavalue * log(x))
+    }
   }
-  return(lambdaDot)
+  return(res)
 }
