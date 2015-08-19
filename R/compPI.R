79b6046c15ca392f2a02cd24ba825b31e62a7d81
Sebastian <szugat@statistik.tu-dortmund.de>
2015-08-19 14:59:10 +0200

    - added new structure of link functions

--- R/compPI.R
+++ R/compPI.R
@@ -5,12 +5,15 @@
 #' @param L integer value indicating the number of events to be predicted (used when xNew not given)
 #' @param L_max integer value indicating the maximum number of events in Poisson process when xNew is not given
 #' @param theta numeric vector of length four with link function's parameters
+#' @param link link function for the negative (!) logarithmized mean of the exponential distribution
+#' @param gradient gradient of link function w.r.t. parameters
+#' @param type if link or gradient are not given, there is collection of implented link functions and gradients, see \code{\link{linkfun}} and \code{\link{gradLambda}}
 #' @param alpha confidence niveau in (0,1)
 #' @param I information matrix
 #' @param withSolve logical value indicating if inverting of information matrix with solve() or ginv() from MASS
 #' @return list with prediction interval bounds and corresponding quantiles of hypoexponential
 #' @export
-compPI <- function(xNew, x0, L, L_max = 35L, theta, alpha, I, withSolve = TRUE) {
+compPI <- function(xNew, x0, L, L_max = 35L, theta, lambda, gradient, type, alpha, I, withSolve = TRUE) {
   if(missing(xNew)) {
     stopifnot(L <= L_max)
     stopifnot(L > length(x0))
@@ -19,18 +22,26 @@ compPI <- function(xNew, x0, L, L_max = 35L, theta, alpha, I, withSolve = TRUE) 
       xNew[i-length(x0)] <- x0[1] * L_max/(L_max - i)
     }
   }
+  
+  if (missing(lambda)) {
+    lambda <- linkfun(type)
+  }
+  
+  if (missing(gradient)) {
+    gradient <- gradLambda(type)
+  }
   if (length(theta) == 2) {
     theta <- c(theta, 0, 1)
   }
-  lambdaNew <- exp(-theta[1] + theta[2]*xNew - theta[3]*xNew^(-theta[4]))
+  lambdaNew <- exp(lambda(x = xNew, theta = theta))
   
-  fitted <- getFit(newdata = xNew, theta = theta)
+  #fitted <- getFit(newdata = xNew, theta = theta, lambda = lambda)
   
   getInterval <- function(rates, newdata) {
     bLower <- qhypoexp(p = alpha/2, rate = rates, interval = c(0, 10^10))
     bUpper <- qhypoexp(p = 1 - alpha/2, rate = rates, interval = c(0, 10^10))
     bDot <- function(y){
-      -1/dhypoexp(x = y, rate = rates) * gradH(newdata, y, theta)
+      -1/dhypoexp(x = y, rate = rates) * gradH(newdata, y, theta, lambda = lambda, gradient = gradient)
     }
     
     if (withSolve) {
