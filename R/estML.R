1ce6c74bc0bae5aca1c6072b59eb4d0cf9000e28
Sebastian <szugat@statistik.tu-dortmund.de>
2015-09-03 13:28:12 +0200

    added gradient

--- R/estML.R
+++ R/estML.R
@@ -4,6 +4,8 @@
 #' @param t values of dependent variable
 #' @param delta vector indicating of length length(t) indicating if the respective observation in t is completely observed (1) or censored (0)
 #' @param link a function(theta, log) used in the likelihood which is \eqn{L(\theta) = \sum t * \lambda(x) - log(\lambda(x))}
+#' @param gradient [\code{function(x, theta, ...)}]\cr
+#'        gradient of link function
 #' @param type [\code{integer}]\cr
 #'        if link function is not given a collection of given link function is available, see \code{\link{linkfun}}
 #' @param start starting solution for optim()
@@ -11,7 +13,7 @@
 #' @return optimal solution for the parameter in the link function in optimum and estimated 
 #'         information matrix in I
 #' @export
-estML <- function(x, t, delta, link, type = 1, start, ...){
+estML <- function(x, t, delta, link, gradient, type = 1, start, ...){
   ## link function
   if (missing(link)) {
     link <- linkfun(type)
@@ -20,12 +22,16 @@ estML <- function(x, t, delta, link, type = 1, start, ...){
   if (missing(delta)) {
     delta <- rep(1, length(t))
   }
+
+  if (missing(gradient)) {
+    gradient <- gradLambda(type)
+  }
   ## objective function
   loglike <- function(theta){
     sum(delta * (t * exp(link(theta = theta, x = x)) - link(theta = theta, x = x)) + # density function
        (1 - delta) * exp(link(theta = theta, x = x)) * t) # survival function
   }
   thetahat <- optim(par = start, fn = loglike, ...)
-  information <- predfat:::estI(x, theta = thetahat$par)
+  information <- estI(x, theta = thetahat$par, lambda = link, gradient = gradient)
   return(list(optimum = thetahat, I = information))
 }
